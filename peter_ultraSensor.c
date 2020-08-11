59fb733fd162edbb6b9d831750f8783df5739d1b
dayoni <kook981@gmail.com>
2020-08-11 14:23:36 +0900

    2020.08.11

--- peter_ultraSensor.c
+++ peter_ultraSensor.c
@@ -1,22 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <wiringPi.h>
-#include <math.h>
-#include <time.h>
-#include <unistd.h>
-#include <signal.h>
-
-#define TRIG 0
-#define ECHO 1
-#define LED1 2
-
-static double first_distance;
-static double last_distance;
-static int isCar;
-static int gs_cnt = 0, ge_cnt = 0;
-
-double ultraSensor();
-void isCorrectObject();
+#include "Peter_ultraSensor.h"
 
 //초음파 시그알람핸들러
 void handler1()
